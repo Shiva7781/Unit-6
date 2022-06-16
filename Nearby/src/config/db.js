da17c5ccad5e3d4bb75b01677f4402fc36a4ec3d
Shiva7781 <97456472+Shiva7781@users.noreply.github.com>
2022-06-16 15:27:18 +0530

    .

--- Nearby/src/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connect = () => {
-    return mongoose.connect(
-        "mongodb+srv://final:final@cluster0.mbty6u5.mongodb.net/nearby?retryWrites=true&w=majority"
-    )
-};
-
-module.exports = connect;
